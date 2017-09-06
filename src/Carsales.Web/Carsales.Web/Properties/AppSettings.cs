455f387d966351733476d70561638718d223411d
Sam Zhou <samzhou.it@gmail.com>
2017-09-07 01:35:55 +0800

    Added apiurl appsettings

---
+++ src/Carsales.Web/Carsales.Web/Properties/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Carsales.Web
+{ 
+    public class AppSettings
+    {
+        public string ApiUrl { get; set; }
+    }
+}
